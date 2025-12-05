e6787ea339af0fac84b7f7d929dda9da7f278750
dior01-dfki <dior01@dfki.de>
2025-12-05 13:53:40 +0100

    .

--- entry.py
+++ entry.py
@@ -1,13 +1,22 @@
 from clearml import Task, Dataset
 import argparse
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
 
 def main(building_id:int):
+    token = os.environ['GIT_OAUTH_TOKEN']
     task = Task.create(
         project_name="ForeSightNEXT/BaltBest",
         task_name="Fetch Building Data Remotely",
         script = "./cml_dataset.py",
         argparse_args=[("bulding_id", building_id)],
-        docker="dior00002/heating-forecast2:v1"
+        docker="dior00002/heating-forecast2:v1",
+                docker_args=(
+            f"-e CLEARML_AGENT_GIT_USER=oauth2 -e CLEARML_AGENT_GIT_PASS={token}"
+        )
     )
     Task.enqueue(task=task, queue_name="default")
 
